2305e2d1210cfae8e5b639e022b12179aa000427
plamenti <plamenti_@abv.bg>
2018-08-18 22:53:21 +0300

    Add hooks demo

---
+++ GameCore/GameCore.Specs/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GameCore.Specs
+{
+    // Inheriting Steps makes Hooks class thread safe 
+    [Binding]
+    public class Hooks : Steps
+    {
+        // using hooks with tags limit the their execution to the scenarios decorated with these specific tags
+        [BeforeScenario("elf")]
+        public void BeforeScenario()
+        {
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+    }
+}
