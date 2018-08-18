0bb340bc26945a6843858420c1641696733f2211
plamenti <plamenti_@abv.bg>
2018-08-18 07:55:08 +0300

    Add PlayerCharacter class

---
+++ GameCore/GameCore/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿namespace GameCore
+{
+    public class PlayerCharacter
+    {
+        public int Health { get; set; } = 100;
+        public bool IsDead { get; set; }
+
+        public void Hit(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+    }
+}
