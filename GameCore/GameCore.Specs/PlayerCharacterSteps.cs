8aaa22c08008b54c3787692539d40043a336d7a6
plamenti <plamenti_@abv.bg>
2018-08-18 08:14:37 +0300

    Add PlayerCharacterSteps class

---
+++ GameCore/GameCore.Specs/PlayerCharacterSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GameCore.Specs
+{
+    [Binding]
+    public class PlayerCharacterSteps
+    {
+        [Given(@"I am a new player")]
+        public void GivenIAmANewPlayer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I take (.*) damage")]
+        public void WhenITakeDamage(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"My health should now be (.*)")]
+        public void ThenMyHealthShouldNowBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
