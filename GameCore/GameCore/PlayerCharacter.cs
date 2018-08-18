8afb8ec4d335fdde3a0016475eee0c2c5b990873
plamenti <plamenti_@abv.bg>
2018-08-18 12:36:41 +0300

    Add resisance logic to PlayerCharacter

--- GameCore/GameCore/PlayerCharacter.cs
+++ GameCore/GameCore/PlayerCharacter.cs
@@ -1,13 +1,26 @@
-﻿namespace GameCore
+﻿using System;
+
+namespace GameCore
 {
     public class PlayerCharacter
     {
         public int Health { get; set; } = 100;
         public bool IsDead { get; set; }
-
+        public int DamageResistance { get; set; }
+        public string Race { get; set; }
+        
         public void Hit(int damage)
         {
-            Health -= damage;
+            int raceSpecificResistance = 0;
+
+            if (Race == "Elf")
+            {
+                raceSpecificResistance = 20;
+            }
+
+            int totalDamageTaken = Math.Max(damage - raceSpecificResistance - DamageResistance, 0);
+
+            Health -= totalDamageTaken;
 
             if (Health <= 0)
             {
