4410e5bfbbae8553bf20a2d9d0f62a1e826082db
plamenti <plamenti_@abv.bg>
2018-08-18 17:17:36 +0300

    Extend PlayerCharacter class

--- GameCore/GameCore/PlayerCharacter.cs
+++ GameCore/GameCore/PlayerCharacter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GameCore
 {
@@ -8,7 +10,26 @@ namespace GameCore
         public bool IsDead { get; set; }
         public int DamageResistance { get; set; }
         public string Race { get; set; }
-        
+        public int MagicPower
+        {
+            get
+            {
+                return MagicalItems.Sum(x => x.Power);
+            }
+        }
+
+        public List<MagicalItem> MagicalItems { get; set; }
+        public List<Weapon> Weapons { get; set; } = new List<Weapon>();
+        public CharacterClass CharacterClass { get; set; }
+        public DateTime LastSleepTime { get; set; }
+        public int WeaponsValue
+        {
+            get
+            {
+                return Weapons.Sum(x => x.Value);
+            }
+        }
+
         public void Hit(int damage)
         {
             int raceSpecificResistance = 0;
@@ -26,6 +47,39 @@ namespace GameCore
             {
                 IsDead = true;
             }
+        }
+
+        public void CastHealingSpell()
+        {
+            if (CharacterClass == CharacterClass.Healer)
+            {
+                Health = 100;
+            }
+            else
+            {
+                Health += 10;
+            }
+        }
+
+        public void ReadHealthScroll()
+        {
+            int daysSinceLastSleep = DateTime.Now.Subtract(LastSleepTime).Days;
+            if (daysSinceLastSleep <= 2)
+            {
+                Health = 100;
+            }
+        }
+
+        public void UseMagicalItem(string itemName)
+        {
+            int powerReduction = 10;
+            if(Race == "Elf")
+            {
+                powerReduction = 0;
+            }
+
+            MagicalItem itemToReduce = MagicalItems.First(item => item.Name == itemName);
+            itemToReduce.Power -= powerReduction;
         }
     }
 }
